0d64f4b49eaf414414a8b1c1a6e849b4128501ee
Kerimov056 <ulvisk@code.edu.az>
2023-05-15 17:40:55 -0700

    first and last

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tabs from './companent/Tabs';
+
+function App() {
+  return (
+    <div className='app'>
+      <Tabs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/companent/Male.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './tabs.css'
+
+const Male = ({imgUrl,name,distace,durum}) => {
+    return (
+        <>
+            <div className='person'>
+                <div className='person_img'>
+                    <img src={imgUrl}></img>
+                </div>
+                <div className='person_name'>
+                    <h5>{name}</h5>
+                    <h6>{distace} Km</h6>
+                </div>
+                <div className='ofline'>
+                    <div className='rapor'>{durum}</div>
+                </div>
+            </div>
+            <hr/>
+        </>
+    )
+}
+
+export default Male
